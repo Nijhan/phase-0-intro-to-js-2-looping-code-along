46f277fcdf824a84125f5fc6e58ff01e80e7b727
Nijhan <najmashaaban6@gmail.com>
2025-06-14 20:25:53 +0300

    objects

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names,eventName){
+    let message=[]
+        for(let i=0;i<names.length;i++){
+            message.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+        }
+        return message
+    
+}
+function countDown(n){
+    while(n>=0){
+        console.log(n)
+        n--
+    }
+}
